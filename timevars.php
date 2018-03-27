8b1a1b398b3bd8e512b9be2f990e2ef875521fdc
TanstGit <37634479+TanstGit@users.noreply.github.com>
2018-03-27 08:51:36 -0500

    Create timevars.php

---
+++ timevars.php
@@ -0,0 +1,14 @@
+<?php
+$CurrentTime1 = date("ymd");       //FormatTime, CurrentTime1, , yyMMdd
+$CurrentTime2 = date("Hi");        //FormatTime, CurrentTime2, , HHmm
+$CurrentTime3 =  date("Ymd");        //FormatTime, CurrentTime3, , yyyyMMdd
+$CurrentTime4 = date("His");       //FormatTime, CurrentTime4, , HHmmss
+$BSN03 = $CurrentTime3;
+$BSN04 = $CurrentTime2;
+$DTM01 = $CurrentTime3;
+$DTM02 = $CurrentTime2;
+$GS04 = $CurrentTime3;
+$GS05 = $CurrentTime4;
+$ISA09 = $CurrentTime1;
+$ISA10 = $CurrentTime2;
+?>
